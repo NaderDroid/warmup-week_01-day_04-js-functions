69adeae535e91beb7d76369f08babe367f7cf1b0
Nader <na511dr@hotmail.com>
2019-04-24 11:09:00 +0300

    Bugs Fixed

--- leapYear.js
+++ leapYear.js
@@ -2,21 +2,29 @@
 
 const leapYear = function (isLeapYear) {
 
-    if (typeof(isLeapYear) === 'number' || isLeapYear > 0) {
+    if (typeof(isLeapYear) === 'number' && isLeapYear > 0) {
 
 
-        if (isLeapYear % 4 === 0 && isLeapYear % 100 === 0 && isLeapYear % 400 === 0) {
-            return "IT IS A LEAP YEAR";
-        } else if (isLeapYear % 4 === 0 && isLeapYear % 100 === 0) {
-            return "IT IS NOT";
-        } else if (isLeapYear % 4 === 0) {
-            return "IT IS A LEAP YEAR";
+        if (isLeapYear % 4 === 0) {
+
+            if (isLeapYear % 4 === 0 && isLeapYear % 100 === 0 && isLeapYear % 400 === 0) {
+
+
+                return "IT IS A LEAP YEAR";
+
+            } else if (isLeapYear % 4 === 0 && isLeapYear % 100 === 0) {
+                return "IT IS NOT";
+            } else {
+                return "IT IS A LEAP YEAR";
+
+            }
+
+        }
+    else
+        {
+            return "NOT VALID NUMBER";
         }
     }
-    else {
-        return "NOT VALID NUMBER";
-    }
-
 };
 
-console.log(leapYear(1900));
+console.log(leapYear(2000));
