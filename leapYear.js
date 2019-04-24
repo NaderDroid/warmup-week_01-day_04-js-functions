9d36536efdd1d992bcc2634b62d623a87d590bbb
Nader <na511dr@hotmail.com>
2019-04-24 10:25:22 +0300

    It is DONE:)

---
+++ leapYear.js
@@ -0,0 +1,22 @@
+
+
+const leapYear = function (isLeapYear) {
+
+    if (typeof(isLeapYear) === 'number' && isLeapYear > 0) {
+
+
+        if (isLeapYear % 4 === 0 && isLeapYear % 100 === 0 && isLeapYear % 400 === 0) {
+            return "IT IS A LEAP YEAR";
+        } else if (isLeapYear % 4 === 0 && isLeapYear % 100 === 0) {
+            return "IT IS NOT";
+        } else if (isLeapYear % 4 === 0) {
+            return "IT IS A LEAP YEAR";
+        }
+    }
+    else {
+        return "NOT VALID NUMBER";
+    }
+
+};
+
+con
